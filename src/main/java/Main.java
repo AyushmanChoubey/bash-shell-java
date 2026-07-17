7e3d69b9b564a9d11d8ced55b751738f721517f8
Ayushman choubey <choubeyayushman6@gmail.com>
2026-07-18 01:33:22 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
+        int n=1;
+        while(n>0){
         Scanner sc=new Scanner(System.in);
         System.out.print("$ ");
         String s=sc.next();
         System.out.println(s+": command not found");
+        }
+        
     }
 }
 
